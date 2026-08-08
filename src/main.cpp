f4f5845f609dded926aefa7194130220c5acd4a0
Paramjot Singh <paramjotdhiman5@gmail.com>
2026-08-08 17:53:38 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
+  std::string command;
 
-  std::string input;
-  std::cin >> input;
-  std::cout << input << ": command not found";
+  std::cout << "$ ";
+  std::cin >> command;
+  std::cout << command << ": command not found";
 }
