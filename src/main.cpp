49e700b43cee8d580ac41d5172b41f0fb998ba76
Paramjot Singh <paramjotdhiman5@gmail.com>
2026-08-09 23:16:03 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,44 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
+#include <cstdlib>
+#include <unistd.h>
+
+std::vector<std::string> split(std::string str, std::string delimiter) {
+  size_t start = 0, pos_end, delime_length = delimiter.length();
+  std::string token;
+  std::vector<std::string> res;
+
+  while((pos_end = str.find(delimiter, start)) != std::string::npos){
+    token = str.substr(start, pos_end - start);
+    start = pos_end + delime_length;
+    res.push_back(token);
+  }
+
+  res.push_back(str.substr(start));
+  return res;
+}
+
+void typeCommand(std::string &input) {
+  auto command = input.substr(5);
+  if(command == "echo" || command == "exit" || command == "type") {
+    std::cout << command <<" is a shell builtin" << std::endl;
+    return;
+  } else {
+    std::string pathVar = std::getenv("PATH");
+    std::vector<std::string> paths = split(pathVar, ":");
+
+    for(auto path: paths){
+      std::string execPath = path + "/" + command;
+      if (access(execPath.c_str(), X_OK) == 0) {
+        std::cout << command << " is " << execPath << std::endl;
+        return;
+      }
+    }
+  }
+  std::cout << command << ": not found" << std::endl;
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -14,12 +53,7 @@ int main() {
     std::getline(std::cin, input);
 
     if(input.substr(0, 5) == "type ") {
-      std::string command = input.substr(5);
-      if(command == "echo" || command == "exit" || command == "type") {
-        std::cout << command <<" is a shell builtin" << std::endl;
-      } else {
-        std::cout << command << ": not found" << std::endl;
-      }
+      typeCommand(input);
     }
     else if (input == "exit") {
       break;
